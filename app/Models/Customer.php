7162df5285d2e457c8bc028b00293d4579dae368
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-21 23:49:49 -0400

    Se definieron los modelos cliente y factura, la relación de uno a muchos, las migraciones y los factories, el resto de componentes se crearon, pero no se han definido

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Customer extends Model
+{
+    /** @use HasFactory<\Database\Factories\CustomerFactory> */
+    use HasFactory;
+
+    protected array $fillable = [
+
+    ];
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,76 +1,8 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('test')->group(function () {
-    Route::get('/', function () {
-        return 'Hola Manuel';
-    });
-
-    Route::post('/{name}', function ($name) {
-        return "Usuario guardado: {$name}";
-    });
-
-    Route::put('/{name}', function ($name) {
-        return "Usuario actualizado: {$name}";
-    });
-
-    Route::patch('/{name}', function ($name) {
-        return "Usuario actualizado: {$name}";
-    });
-
-    Route::delete('/{name}', function ($name) {
-        return "Usuario borrado: {$name}";
-    });
-});
-
-Route::get('random/{min}/{max}', function ($min, $max) {
-    $data = [];
-    $code = 200;
-    
-    if(is_numeric($min) && is_numeric($max)) {
-        $random_number = random_int($min, $max);
-        $data = ['random_number' => $random_number];
-    }
-    else 
-    {
-        $data = ['error' => 'El tipo de dato ingresado no es el correcto'];
-        $code = 400;
-    }
-
-    return response()->json($data, $code);
-});
-
-Route::prefix('users')->group(function () {
-    Route::get('/', function () {
-        $users = User::all();
-        return response()->json(['users' => $users], 200);
-    });
-
-    Route::post('/', function (Request $request) {
-        $data = $request->all();
-        $data['password'] = '$2a$12$w6R6DlbSE.pl40v95bTyuuUofpqzxDvBlNUh2ka4JrYR3eKX3Xj8y'; //1234
-
-        $user = User::create($data);
-        
-        return response()->json(['new-user' => $user], 200);
-    });
-});
-
-Route::prefix('v2/users')->group(function () {
-    Route::get('/', function () {
-        $users = User::select('name', 'email')->get();
-        return response()->json(['users' => $users], 200);
-    });
-
-    Route::post('/', function (Request $request) {
-        $data = $request->all();
-        $data['password'] = bcrypt($data['password']);
-
-        $user = User::create($data);
-        
-        return response()->json(['new-user' => $user], 200);
-    });
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
